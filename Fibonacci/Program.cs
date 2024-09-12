4bb0a485484b475dbf477e13d618e3d0b738f191
ashikur rahman <nillakash4097@gmail.com>
2024-09-12 10:09:48 +0600

    Coomit

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int p1 = 0, p2 = 1;
+int count;
+Console.Write("Enter The Number");
+count = Int32.Parse(Console.ReadLine());
+
+Console.Write(p1 +" " +p2);
+Fibonacci(count - 2,p1,p2);
+Console.WriteLine();
+//for (int i = 2; i < count; i++)
+//{
+//    p3 = p1 + p2;
+//    Console.Write( " " +p3);
+//    p1 = p2;
+//    p2 = p3;
+//}
+
+static void Fibonacci(int cnt,int p1,int p2)
+{
+    int p3;
+    if (cnt > 0)
+    {
+        p3 = p1 + p2;
+        Console.Write(" " +p3);
+        p1 = p2;
+        p2 = p3;
+        Fibonacci(cnt - 1,p1,p2);
+    }
+}
