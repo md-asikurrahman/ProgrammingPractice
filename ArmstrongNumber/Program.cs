7e32007f2fed66abad5113c4feef37c30b6d16bd
ashikur rahman <nillakash4097@gmail.com>
2024-10-15 12:01:47 +0600

    modified

---
+++ ArmstrongNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿int num, sum = 0, temp, rem;
+Console.WriteLine("Enter the Number : ");
+num = int.Parse(Console.ReadLine());
+
+temp = num;
+while (num > 0)
+{
+    rem = num % 10;
+    sum = sum +( rem * rem * rem);
+    num = num / 10;
+}
+if (temp == sum)
+{
+    Console.WriteLine("Armstrong Number");
+}
+else
+{
+    Console.WriteLine("Not Armstrong Number");
+    Console.ReadLine();
+}
